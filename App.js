1c81346f9b315dc9fd2f1582d9052f385191b675
lucasrbrusu <elucas2005@icloud.com>
2025-12-06 02:13:15 +0000

    first clone

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { AppProvider } from './src/context/AppContext';
+import Navigation from './src/navigation';
+
+export default function App() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <AppProvider>
+          <StatusBar style="dark" />
+          <Navigation />
+        </AppProvider>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
+  );
+}
