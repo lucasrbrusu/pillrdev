3f3c7335b2f37dfaa40d562fe34e2858ee34a928
Lucas <elucas2005@icloud.com>
2025-12-15 16:37:11 +0000

    more android build changes

--- src/navigation/index.android.js
+++ src/navigation/index.android.js
@@ -41,7 +41,7 @@ const TabBarIcon = ({ name, type, color, size }) => {
 
 const CustomTabBar = ({ state, descriptors, navigation, styles }) => {
   const insets = useSafeAreaInsets();
-  const bottomPadding = insets.bottom > 0 ? insets.bottom : 2;
+  const bottomPadding = (insets.bottom || 0) + 6;
 
   return (
     <View style={[styles.tabBarContainer, { paddingBottom: bottomPadding }]}>
